d95315bc1ba6d9a88dfe371afb23c29daa2eb053
Nick Smallbone <nick@smallbone.se>
2017-12-22 19:56:12 +0000

    Change query

--- Query.hs
+++ Query.hs
@@ -8,7 +8,7 @@ import Data.Reflection
 
 query ::
   WithCorpus BNC => 
-  Lemma BNC -> Lemma BNC -> Lemma BNC -> [Sentence BNC]
+  Lemma BNC -> Lemma BNC -> POS BNC -> [Sentence BNC]
 query w1 w2 w3 =
   map getSentence $
   IntSet.toList $ IntSet.fromList $ do
@@ -17,11 +17,11 @@ query w1 w2 w3 =
     (i, w1_is) <- toList w1_occ
     let w2_occ = filterGT i (lemmaIndex ! w2)
     (j, w2_js) <- toList w2_occ
-    let w3_occ = lemmaIndex ! w3 ! (j+1)
+    let w3_occ = posIndex ! w3 ! (j+1)
     -- (k, w3_ks) <- toList w3_occ
     map token_sentence (Vector.toList (get (intersection [w1_is, w2_js, w3_occ])))
 
 main = do
   corpus <- loadCorpus
   withCorpus corpus $
-    timeIt (mapM_ (putStrLn . showSentence) (query "cat" "the" "dog"))
+    timeIt (mapM_ (putStrLn . showSentence) (query "cat" "the" "SUBST"))
